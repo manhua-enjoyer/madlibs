ef3e0e5206088b13ff865612b500fc9122d79c1c
manhua-enjoyer <102496414+manhua-enjoyer@users.noreply.github.com>
2022-04-02 09:15:26 -0700

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,10 @@
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Second Verb: ")
+anything = input("Any word you want: ")
+
+Story1 = f"Coding is just such a {adj} thing. Everytime I {verb1} about coding, it just make me want to {verb2}. \
+In the future of my career, I want to become a {anything} of coding!"
+
+print(Story1)
+
